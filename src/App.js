148d5ff419c97236f311436747c3cf8deed011d0
LookerKy <nagaraz07@gmail.com>
2020-01-08 16:37:54 +0900

    make redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 
 import './App.css';
-import Counter from './components/Counter';
-import Palette from './components/Palette';
-import WaitingList from './components/WaitingList';
+import CounterContainer from './containers/CounterContainer';
+import PaletteContainer from './containers/PaletteContainer';
+import WaitingContainer from './containers/WaitingContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Palette selected="red" />
-        <Counter value={0} color="red" />
-        <WaitingList />
+        <PaletteContainer />
+        <CounterContainer />
+        <WaitingContainer />
       </div>
     );
   }
---
+++ src/store/modules/waiting.js
@@ -0,0 +1,45 @@
+import { createAction, handleActions } from 'redux-actions';
+
+const CHANGE_INPUT = 'waiting/CHANGE_INPUT'; // 인풋 값 변경
+const CREATE = 'waiting/CREATE'; // 명단에 이름 추가
+const ENTER = 'waiting/ENTER'; // 입장
+const LEAVE = 'waiting/LEAVE'; // 나감
+
+let id = 0;
+export const changeInput = createAction(CHANGE_INPUT, text => text);
+export const create = createAction(CREATE, text => ({ text, id: id++ }));
+export const enter = createAction(ENTER, id => id);
+export const leave = createAction(LEAVE, id => id);
+
+const initialState = {
+  input: '',
+  list: [],
+};
+
+export default handleActions(
+  {
+    [CHANGE_INPUT]: (state, action) => ({
+      ...state,
+      input: action.payload,
+    }),
+    [CREATE]: (state, action) => ({
+      ...state,
+      list: state.list.concat({
+        id: action.payload.id,
+        name: action.payload.text,
+        entered: false,
+      }),
+    }),
+    [ENTER]: (state, action) => ({
+      ...state,
+      list: state.list.map(item =>
+        item.id === action.payload ? { ...item, entered: !item.entered } : item
+      ),
+    }),
+    [LEAVE]: (state, action) => ({
+      ...state,
+      list: state.list.filter(item => item.id !== action.payload),
+    }),
+  },
+  initialState
+);
